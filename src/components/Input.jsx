abead4029eefeac76a940d0dde245f8202fbbf3c
denis-larin-22 <denislarin2017@gmail.com>
2023-03-20 17:52:56 +0200

    fixed inputValue

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,23 +1,18 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { InputContext } from "../App";
 
 const Input = () => {
     const { string, setString } = useContext(InputContext);
-
-    const input = () => {
-        const input = document.querySelector('input');
-        return input.value;
-    }
+    const [inputValue, setInputValue] = useState('');
 
     return (
         <form className="input" >
             <h3>Input</h3>
-            <input type="text" placeholder="Write a text" />
+            <input type="text" placeholder="Write a text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
 
             <button onClick={(e) => {
                 e.preventDefault();
-                let result = input();
-                setString(result);
+                setString(inputValue);
             }}>Submit</button>
 
             <h2>Input text: {string}</h2>
